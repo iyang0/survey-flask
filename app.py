121060b322344d1ec1789bfbe6f1ccc48dbc1964
magus0 <harvestashya@gmail.com>
2021-07-08 12:26:27 -0700

    survey works

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ responses = []
 
 @app.route("/")
 def root():
+    """base case, start a survey"""
     print(survey.instructions)
     return render_template("survey_start.html",
         title = survey.title,
@@ -18,16 +19,27 @@ def root():
         
 @app.route("/begin", methods=["POST"])
 def survey_redirect():
+    """redirect after starting the survey"""
     return redirect("/questions/0")
 
 @app.route("/questions/<int:question_index>")
 def questions(question_index):
+    """ask question from the survey based on how many answered before"""
     print(responses)
     return render_template("question.html",
         question = survey.questions[question_index])
 
 @app.route("/answer",methods=["POST"])
 def answer_redirect():
+    """appends response with the user's answer and redirects to another question or a thank you page"""
     answer = request.form.get("answer")
     responses.append(answer)
-    return redirect(f"/questions/{len(responses)}")
+    if len(responses) >= len(survey.questions):
+        return redirect("/thanks")
+    else:
+        return redirect(f"/questions/{len(responses)}")
+
+@app.route("/thanks")
+def completion():
+    """render thank you page when survey completed"""
+    return render_template("completion.html")
