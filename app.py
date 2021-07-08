55f3ae24d9f7f0683bd0a3a0785355364543c57d
magus0 <harvestashya@gmail.com>
2021-07-08 16:38:05 -0700

    complete functionality

--- app.py
+++ app.py
@@ -25,9 +25,16 @@ def survey_redirect():
 @app.route("/questions/<int:question_index>")
 def questions(question_index):
     """ask question from the survey based on how many answered before"""
+    
+    
     if question_index != len(session["responses"]):
+        #if user tries to access question out of order, redirect them to correct question.
+        flash("You tried to access an invalid question")
         return redirect(f"/questions/{len(session['responses'])}")
+        
     elif len(session["responses"]) == len(survey.questions):
+        #if user tries to access a question after completing the survey redirect them back to the thanks
+        flash("You have already finished the survey")
         return redirect("/thanks")
     else:
         return render_template("question.html",
