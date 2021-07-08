8cb8846200753f871c739d235eba073b3d18348c
Grant <grantyli94@gmail.com>
2021-07-08 18:58:30 -0400

    up to step 8

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
-responses = []
+
 
 @app.route("/")
 def root():
     """base case, start a survey"""
-    print(survey.instructions)
+    session["responses"] = []
     return render_template("survey_start.html",
         title = survey.title,
         instructions=survey.instructions)
@@ -25,15 +25,23 @@ def survey_redirect():
 @app.route("/questions/<int:question_index>")
 def questions(question_index):
     """ask question from the survey based on how many answered before"""
-    print(responses)
-    return render_template("question.html",
-        question=survey.questions[question_index])
+    if question_index != len(session["responses"]):
+        return redirect(f"/questions/{len(session['responses'])}")
+    elif len(session["responses"]) == len(survey.questions):
+        return redirect("/thanks")
+    else:
+        return render_template("question.html",
+            question=survey.questions[question_index])
 
 @app.route("/answer",methods=["POST"])
 def answer_redirect():
     """appends response with the user's answer and redirects to another question or a thank you page"""
     answer = request.form["answer"]
+    
+    responses = session["responses"]
     responses.append(answer)
+    session["responses"] = responses
+    
     if len(responses) >= len(survey.questions):
         return redirect("/thanks")
     else:
@@ -42,4 +50,5 @@ def answer_redirect():
 @app.route("/thanks")
 def completion():
     """render thank you page when survey completed"""
+    print(session["responses"])
     return render_template("completion.html")
