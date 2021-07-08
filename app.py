34dfdaa38bbc9a5ec0b4a8246f993885bb01dad1
magus0 <harvestashya@gmail.com>
2021-07-08 11:46:50 -0700

    exploratory stuff

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+responses = []
+
+@app.route("/")
+def root():
+    print(survey.instructions)
+    return render_template("survey_start.html",
+        title = survey.title,
+        instructions=survey.instructions)
+        
+@app.route("/begin")
+def survey_redirect():
+    return redirect("/questions/0")
+
+@app.route("/questions/<question_index>")
+def questions(question_index):
+    #TODO add questions
+    return render_template("question.html")
