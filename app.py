9dfe4d31a33e10f00e7474dcd3eaccaf11469988
magus0 <harvestashya@gmail.com>
2021-07-08 17:45:20 -0700

    code reviewed, has multiple surveys

--- app.py
+++ app.py
@@ -21,37 +21,40 @@ def root():
 @app.route("/begin", methods=["POST"])
 def survey_redirect():
     """redirect after starting the survey"""
-    session["survey"] = surveys[request.args["survey"]]
+    session["survey_key"] = request.form["survey"]
     return redirect("/questions/0")
 
 @app.route("/questions/<int:question_index>")
 def questions(question_index):
     """ask question from the survey based on how many answered before"""
-    
+    survey = surveys[session["survey_key"]]
     
     if question_index != len(session["responses"]):
         #if user tries to access question out of order, redirect them to correct question.
         flash("You tried to access an invalid question")
         return redirect(f"/questions/{len(session['responses'])}")
         
-    elif len(session["responses"]) == len(session["survey"].questions):
+    elif len(session["responses"]) == len(survey.questions):
         #if user tries to access a question after completing the survey redirect them back to the thanks
         flash("You have already finished the survey")
         return redirect("/thanks")
     else:
         return render_template("question.html",
-            question=session["survey"].questions[question_index])
+            question=survey.questions[question_index])
 
 @app.route("/answer",methods=["POST"])
 def answer_redirect():
     """appends response with the user's answer and redirects to another question or a thank you page"""
     answer = request.form["answer"]
-    
+    comment = request.form.get("comment")
+    if comment:
+        answer = (answer, comment)
     responses = session["responses"]
     responses.append(answer)
     session["responses"] = responses
+    survey = surveys[session["survey_key"]]
     
-    if len(responses) >= len(session["survey"].questions):
+    if len(responses) >= len(survey.questions):
         return redirect("/thanks")
     else:
         return redirect(f"/questions/{len(responses)}")
